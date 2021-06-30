95059020ba623ad756726a73b3af06ee45ace352
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-06-30 12:26:36 +0100

    remove unused loss

--- bolts/losses/loss.py
+++ bolts/losses/loss.py
@@ -1,15 +1,9 @@
 from torch import Tensor, nn
 
-__all__ = ["CrossEntropy", "L1"]
+__all__ = ["CrossEntropy"]
 
 
 class CrossEntropy(nn.CrossEntropyLoss):
     def forward(self, input: Tensor, target: Tensor) -> Tensor:
         _target = target.view(-1).long()
-        return super().forward(input, _target)
-
-
-class L1(nn.L1Loss):
-    def forward(self, input: Tensor, target: Tensor) -> Tensor:
-        _target = target.view_as(input).float()
         return super().forward(input, _target)
