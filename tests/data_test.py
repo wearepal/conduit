40d5372ac8c7313663b4b2e186fb4c17310e9186
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-07-02 11:06:46 +0100

    Rename test_mnist to test_datasets.

--- tests/data_test.py
+++ tests/data_test.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import pytest
-from torchvision import transforms as TF
+from torchvision import transforms as T
 from torchvision.datasets import VisionDataset
 
 from bolts.data.datasets import ISIC, MNIST
@@ -9,11 +9,11 @@ from bolts.data.datasets import ISIC, MNIST
 
 @pytest.mark.slow
 @pytest.mark.parametrize("ds_cls", [MNIST, ISIC])
-def test_mnist(ds_cls: type[VisionDataset]) -> None:
+def test_datasets(ds_cls: type[VisionDataset]) -> None:
     """Basic test for datasets.
     Confirms that the datasets can be instantiated and have a functional __getitem__ method.
     """
-    transform = TF.ToTensor()
+    transform = T.ToTensor()
     ds = ds_cls(root=".")
     train_ds = MNIST(root=".", train=True, transform=transform)
     test_ds = MNIST(root=".", train=True, transform=transform)
