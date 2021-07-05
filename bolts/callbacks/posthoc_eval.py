ea2fccff6cd03dc5c9975131fc8f21ef2eb12b69
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-07-05 13:29:17 +0100

    wip. Open pr to get feedback

--- bolts/callbacks/posthoc_eval.py
+++ bolts/callbacks/posthoc_eval.py
@@ -1,11 +1,55 @@
 from collections import Sequence
+from typing import NamedTuple, Optional
 
 from kit import gcopy
 import pytorch_lightning as pl
-from pytorch_lightning.utilities.types import STEP_OUTPUT
+from pytorch_lightning.utilities.types import EPOCH_OUTPUT, STEP_OUTPUT
+import torch
+from torch import Tensor, nn
+from torch.optim import AdamW
 from torch.utils.data import DataLoader
 
 __all__ = ["PostHocEval"]
+
+
+class EvalModule(pl.LightningModule):
+    def __init__(self, enc: nn.Module, clf: nn.Module):
+        super().__init__()
+        self.enc = enc
+        self.clf = gcopy(clf)
+        self._loss = nn.CrossEntropyLoss()
+
+    def training_step(self, batch: NamedTuple, batch_idx: int) -> STEP_OUTPUT:
+        y = self(batch.x)
+        return self._loss(y, batch.y.view(-1).long())
+
+    def forward(self, x: Tensor) -> Tensor:
+        with torch.no_grad():
+            self.enc.eval()
+            z = self.enc(x)
+        return self.clf(z)
+
+    def test_step(self, batch: NamedTuple, batch_idx: int) -> Optional[STEP_OUTPUT]:
+        logits = self(batch.x)
+        return {
+            "y": batch.y.view(-1),
+            "preds": logits.sigmoid().round().squeeze(-1),
+        }
+
+    def test_epoch_end(self, outputs: EPOCH_OUTPUT) -> None:
+        torch.cat([_r["y"] for _r in outputs], 0)
+        torch.cat([_r["preds"] for _r in outputs], 0)
+
+    @staticmethod
+    def _maybe_reset_parameters(module: nn.Module) -> None:
+        if hasattr(module, 'reset_parameters'):
+            module.reset_parameters()  # type: ignore
+
+    def reset_parameters(self):
+        self.clf.apply(self._maybe_reset_parameters)
+
+    def configure_optimizers(self):
+        return AdamW(self.clf.parameters())
 
 
 class PostHocEval(pl.Callback):
@@ -13,6 +57,7 @@ class PostHocEval(pl.Callback):
     Trains and evaluates an auxillary classifier model.
     Your model should have:
         - ``self.classifier``
+        - ``self.encoder``
         - ``self.datamodule``
         - ``self.clf_epochs``
         - ``self.trainer''
@@ -22,8 +67,11 @@ class PostHocEval(pl.Callback):
         super().__init__()
 
     def on_pretrain_routine_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
-        pl_module.classifier = pl_module.classifier.to(pl_module.device)
-        pl_module.eval_trainer = gcopy(trainer, max_epochs=pl_module.clf_epochs)
+        pl_module.eval_trainer = gcopy(
+            trainer,
+            max_epochs=pl_module.clf_epochs,
+            callbacks=[cb for cb in trainer.callbacks if not isinstance(cb, PostHocEval)],
+        )
 
     def _eval_loop(
         self,
@@ -33,12 +81,13 @@ class PostHocEval(pl.Callback):
         test_dl: DataLoader,
     ) -> None:
         trainer.fit(model, train_dataloader=train_dl)
-        trainer.test(dataloaders=test_dl)
+        trainer.test(test_dataloaders=test_dl)
 
     def _call_eval_loop(self, pl_module: pl.LightningModule):
+        pl_module.reset_parameters()
         self._eval_loop(
             trainer=pl_module.eval_trainer,
-            model=pl_module.classifier,
+            model=EvalModule(enc=pl_module.encoder, clf=pl_module.classifier),
             train_dl=pl_module.datamodule.train_dataloader(),
             test_dl=pl_module.datamodule.val_dataloader(),
         )
@@ -58,9 +107,10 @@ class PostHocEval(pl.Callback):
         self._call_eval_loop(pl_module)
 
     def on_test_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        pl_module.reset_parameters()
         self._eval_loop(
             trainer=pl_module.eval_trainer,
-            model=pl_module.classifier,
+            model=EvalModule(enc=pl_module.encoder, clf=pl_module.classifier),
             train_dl=pl_module.datamodule.train_dataloader(),
             test_dl=pl_module.datamodule.test_dataloader(),
         )
--- tests/callbacks_test.py
+++ tests/callbacks_test.py
@@ -1,5 +1,5 @@
-from pl_bolts.models import LogisticRegression
 import pytorch_lightning as pl
+from torch import nn
 
 from bolts.callbacks import PostHocEval
 from bolts.fair.models import ErmBaseline
@@ -8,12 +8,13 @@ from tests.fair.model_test import DummyDataModule, Encoder
 
 def test_post_hoc_eval():
     """Test the post hoc eval callback."""
-    trainer = pl.Trainer(fast_dev_run=True)
+    trainer = pl.Trainer(max_steps=1)
     enc = Encoder(input_shape=(3, 64, 64), initial_hidden_channels=64, levels=3, encoding_dim=128)
-    clf = LogisticRegression(input_dim=128, num_classes=2)
+    clf = nn.Sequential(nn.Flatten(), nn.Linear(128, 2))
     dm = DummyDataModule()
     model = ErmBaseline(enc=enc, clf=clf, weight_decay=1e-8, lr=1e-3, lr_gamma=0.999)
     model.classifier = model.clf
+    model.encoder = model.enc
     model.clf_epochs = 1
     model.datamodule = dm
     model.batch_size_eval = 10
