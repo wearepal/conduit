dde33bbb508fd30bc57c8e9d3f1c069a19f407a6
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-08-06 16:39:08 +0100

    appease myles

--- bolts/data/structures.py
+++ bolts/data/structures.py
@@ -31,15 +31,46 @@ class NamedSample:
     def __len__(self) -> int:
         return len(self.__dataclass_fields__)  # type: ignore[attr-defined]
 
+    def add_field(
+        self, y: Tensor | None = None, s: Tensor | None = None, iw: Tensor | None = None
+    ) -> NamedSample | BinarySample | BinarySampleIW | TernarySample | TernarySampleIW:
+        if y is not None:
+            if s is not None:
+                if iw is not None:
+                    return TernarySampleIW(x=self.x, s=s, y=y, iw=iw)
+                return TernarySample(x=self.x, s=s, y=y)
+            if iw is not None:
+                return BinarySampleIW(x=self.x, y=y, iw=iw)
+            return BinarySample(x=self.x, y=y)
+        return self
+
 
 @dataclass(frozen=True)
 class BinarySample(NamedSample):
     y: Tensor
 
+    def add_field(
+        self, y: Tensor | None = None, s: Tensor | None = None, iw: Tensor | None = None
+    ) -> BinarySample | BinarySampleIW | TernarySample | TernarySampleIW:
+        if s is not None:
+            if iw is not None:
+                return TernarySampleIW(x=self.x, s=s, y=self.y, iw=iw)
+            return TernarySample(x=self.x, s=s, y=self.y)
+        if iw is not None:
+            return BinarySampleIW(x=self.x, y=self.y, iw=iw)
+        return self
+
 
 @dataclass(frozen=True)
 class BinarySampleIW(BinarySample):
     iw: Tensor
+
+    def add_field(
+        self, y: Tensor | None = None, s: Tensor | None = None, iw: Tensor | None = None
+    ) -> BinarySampleIW | TernarySampleIW:
+        if s is not None:
+            return TernarySampleIW(x=self.x, s=s, y=self.y, iw=self.iw)
+        return self
 
 
 @dataclass(frozen=True)
@@ -47,10 +78,22 @@ class TernarySample(BinarySample):
     y: Tensor
     s: Tensor
 
+    def add_field(
+        self, y: Tensor | None = None, s: Tensor | None = None, iw: Tensor | None = None
+    ) -> TernarySample | TernarySampleIW:
+        if iw is not None:
+            return TernarySampleIW(x=self.x, s=self.s, y=self.y, iw=iw)
+        return self
+
 
 @dataclass(frozen=True)
 class TernarySampleIW(TernarySample):
     iw: Tensor
+
+    def add_field(
+        self, y: Tensor | None = None, s: Tensor | None = None, iw: Tensor | None = None
+    ) -> TernarySampleIW:
+        return self
 
 
 class InputSize(NamedTuple):
--- tests/data_test.py
+++ tests/data_test.py
@@ -2,9 +2,17 @@ from __future__ import annotations
 from pathlib import Path
 
 import pytest
+import torch
 from torchvision import transforms as T
 from torchvision.datasets import VisionDataset
 
+from bolts.data import (
+    BinarySample,
+    BinarySampleIW,
+    NamedSample,
+    TernarySample,
+    TernarySampleIW,
+)
 from bolts.data.datasets import ISIC, ColoredMNIST
 
 
@@ -18,3 +26,29 @@ def test_datasets(ds_cls: type[VisionDataset]) -> None:
     ds = ds_cls(root=Path("~/Data").expanduser(), transform=transform)
     for _ds in ds:
         assert _ds[0] is not None
+
+
+def test_add_field():
+    x = torch.rand(3, 2)
+    s = torch.randint(0, 2, (1, 2))
+    y = torch.randint(0, 2, (1, 2))
+    iw = torch.rand(1, 2)
+    ns = NamedSample(x)
+    assert isinstance(ns.add_field(), NamedSample)
+    assert isinstance(ns.add_field(y=y), BinarySample)
+    assert isinstance(ns.add_field(y=y, iw=iw), BinarySampleIW)
+    assert isinstance(ns.add_field(y=y, s=s), TernarySample)
+    assert isinstance(ns.add_field(y=y, s=s, iw=iw), TernarySampleIW)
+    bs = BinarySample(x=x, y=y)
+    assert isinstance(bs.add_field(), BinarySample)
+    assert isinstance(bs.add_field(iw=iw), BinarySampleIW)
+    assert isinstance(bs.add_field(s=s), TernarySample)
+    assert isinstance(bs.add_field(s=s, iw=iw), TernarySampleIW)
+    bsi = BinarySampleIW(x=x, y=y, iw=iw)
+    assert isinstance(bsi.add_field(), BinarySampleIW)
+    assert isinstance(bsi.add_field(s=s), TernarySampleIW)
+    ts = TernarySample(x=x, s=s, y=y)
+    assert isinstance(ts.add_field(), TernarySample)
+    assert isinstance(ts.add_field(iw=iw), TernarySampleIW)
+    tsi = TernarySampleIW(x=x, s=s, y=y, iw=iw)
+    assert isinstance(tsi.add_field(), TernarySampleIW)
