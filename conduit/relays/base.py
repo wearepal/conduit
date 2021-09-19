2caf55314076ff88f23f8a1f380c375b26fd2e43
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-09-20 00:27:27 +0100

    Allow Relay to accept non-Conf classes.

--- conduit/relays/base.py
+++ conduit/relays/base.py
@@ -1,10 +1,13 @@
 from abc import abstractmethod
+from collections import defaultdict
+from dataclasses import is_dataclass
 import importlib
 import logging
 import os
 from pathlib import Path
 import re
 import sys
+from types import ModuleType
 from typing import (
     Any,
     ClassVar,
@@ -13,6 +16,7 @@ from typing import (
     NamedTuple,
     Optional,
     Sequence,
+    Tuple,
     Type,
     TypeVar,
     Union,
@@ -39,7 +43,13 @@ __all__ = [
 
 class SchemaInfo(NamedTuple):
     name: str
-    conf: Type
+    conf: Type[Any]
+
+
+class _SchemaImportInfo(NamedTuple):
+    name: str
+    class_name: str
+    module: ModuleType
 
 
 R = TypeVar("R", bound="Relay")
@@ -48,8 +58,8 @@ R = TypeVar("R", bound="Relay")
 @attr.define(kw_only=True)
 class Relay:
     _CONFIG_NAME: ClassVar[str] = "config"
-    _SCHEMA_NAME: ClassVar[str] = "experiment_schema"
-    _CONFIGEN_FILENAME: ClassVar[str] = ".conf"
+    _PRIMARY_SCHEMA_NAME: ClassVar[str] = "relay_schema"
+    _CONFIGEN_FILENAME: ClassVar[str] = "conf.py"
     _logger: ClassVar[Optional[logging.Logger]] = None
 
     @classmethod
@@ -74,13 +84,14 @@ class Relay:
     def _init_config_dir(
         cls: Type[R], *, config_dir: Path, config_dict: Dict[str, Sequence[Any]]
     ) -> None:
+        # TODO: Make work for subsequent runs when new options are added
         config_dir.mkdir(parents=True)
         cls.log(f"Initialising config directory '{config_dir}'")
         indent = "  "
         with open((config_dir / cls._CONFIG_NAME).with_suffix(".yaml"), "w") as exp_config:
             cls.log(f"Initialising primary config file '{exp_config.name}'")
             exp_config.write(f"defaults:")
-            exp_config.write(f"\n{indent}- {cls._SCHEMA_NAME}")
+            exp_config.write(f"\n{indent}- {cls._PRIMARY_SCHEMA_NAME}")
 
             for group, schema_ls in config_dict.items():
                 group_dir = config_dir / group
@@ -99,36 +110,98 @@ class Relay:
         cls.log(f"Finished initialising config directory initialised at '{config_dir}'")
 
     @classmethod
-    def _create_conf(cls: Type[R], config_dir: Path) -> None:
+    def _generate_conf(
+        cls: Type[R], output_dir: Path, *, module_class_dict: Dict[str, List[str]]
+    ) -> None:
         from configen.config import ConfigenConf, ModuleConf  # type: ignore
         from configen.configen import generate_module, save  # type: ignore
 
         cfg = ConfigenConf(
-            output_dir=str(config_dir),
-            module_path_pattern="{{module_path}}" + f"/{cls._CONFIGEN_FILENAME}.py",
+            output_dir=str(output_dir),
+            module_path_pattern="{{module_path}}" + f"/{cls._CONFIGEN_FILENAME}",
             modules=[],
             header="",
         )
-        module_conf = ModuleConf(name=cls.__module__, classes=[cls.__name__])
-        code = generate_module(cfg=cfg, module=module_conf)
-        save(cfg=cfg, module=str(cfg.output_dir), code=code)
+        # for module, class in cfg.modules:
+        for module, classes in module_class_dict.items():
+            module_conf = ModuleConf(name=module, classes=classes)
+            code = generate_module(cfg=cfg, module=module_conf)
+            # TODO: Change saving to append if file already exists
+            save(cfg=cfg, module=module, code=code)
 
     @classmethod
-    def _get_config_class(cls: Type[R], config_dir: Path) -> Any:
-        conf_class_file = (config_dir / cls._CONFIGEN_FILENAME).with_suffix(".py")
-        if not (conf_class_file).exists():
-            cls._create_conf(config_dir=config_dir)
+    def _load_module_from_path(cls: Type[R], filepath: Path) -> ModuleType:
         spec = importlib.util.spec_from_file_location(  # type: ignore
-            name=conf_class_file.name, location=str(conf_class_file)
+            name=filepath.name, location=str(filepath)
         )
         module = importlib.util.module_from_spec(spec)  # type: ignore
         spec.loader.exec_module(module)
-        sys.modules[conf_class_file.name] = module
-        return getattr(module, f"{cls.__name__}Conf")
+        sys.modules[filepath.name] = module
+        return module
+
+    @classmethod
+    def _get_schemas(
+        cls: Type[R], config_dir: Path, **options: Sequence[SchemaInfo]
+    ) -> Tuple[Type[Any], Dict[str, List[SchemaInfo]]]:
+        configen_dir = config_dir / "configen"
+        module_path_ps = configen_dir / cls.__module__.replace(".", "/") / cls._CONFIGEN_FILENAME
+        to_generate = defaultdict(list)
+        if not module_path_ps.exists():
+            to_generate[cls.__module__].append(cls.__name__)
+
+        imported_schemas = defaultdict(list)
+        to_import = defaultdict(list)
+
+        for group, classes in options.items():
+            for info in classes:
+                module_path_ss = (
+                    configen_dir / info.conf.__module__.replace(".", "/") / cls._CONFIGEN_FILENAME
+                )
+                module = (
+                    cls._load_module_from_path(module_path_ss) if module_path_ss.exists() else None
+                )
+
+                cls_name = info.conf.__name__
+                if (not is_dataclass(info.conf)) or (not cls_name.endswith("Conf")):
+                    schema_name = f"{cls_name}Conf"
+                    schema_missing = False
+                    if module is None:
+                        schema_missing = True
+                    else:
+                        schema = getattr(module, schema_name, None)
+                        if schema is None:
+                            schema_missing = True
+                        else:
+                            imported_schemas[group].append(SchemaInfo(name=info.name, conf=schema))
+                    if schema_missing:
+                        to_generate[info.conf.__module__].append(cls_name)
+                    import_info = _SchemaImportInfo(
+                        name=info.name, class_name=schema_name, module=module_path_ss
+                    )
+                    to_import[group].append(import_info)
+                else:
+                    imported_schemas[group].append(info)
+
+        # Generate any confs with configen that have yet to be generated
+        if to_generate:
+            cls._generate_conf(output_dir=configen_dir, module_class_dict=to_generate)
+
+        # Load the primary schema
+        module = cls._load_module_from_path(module_path_ps)
+        primary_schema = getattr(module, cls.__name__ + "Conf")
+        # Load the sub-schemas
+        for group, names in to_import.items():
+            for name, schema_cls_name, module_path in names:
+                module = cls._load_module_from_path(module_path)
+                imported_schemas[group].append(
+                    SchemaInfo(name=name, conf=getattr(module, schema_cls_name))
+                )
+
+        return primary_schema, imported_schemas
 
     @classmethod
     def with_hydra(
-        cls: Type[R],
+        cls: Type,
         base_config_dir: Union[Path, str],
         **schemas: SchemaInfo,
     ) -> None:
@@ -137,7 +210,7 @@ class Relay:
     @final
     @classmethod
     def _launch(
-        cls: Type[R], *, base_config_dir: Union[Path, str], **schemas: Sequence[SchemaInfo]
+        cls: Type[R], *, base_config_dir: Union[Path, str], **options: Sequence[SchemaInfo]
     ) -> None:
         base_config_dir = Path(base_config_dir)
         config_dir_name = cls._config_dir_name()
@@ -148,13 +221,13 @@ class Relay:
                 f"Config directory {config_dir} not found."
                 "\nInitialising directory based on the supplied conf classes."
             )
-            cls._init_config_dir(config_dir=config_dir, config_dict=schemas)
+            cls._init_config_dir(config_dir=config_dir, config_dict=options)
             cls.log(f"Relaunch the relay, modifying the config files first if desired.")
             return
 
-        config_class = cls._get_config_class(config_dir=config_dir)
+        primary_schema, schemas = cls._get_schemas(config_dir=config_dir, **options)
         sr = SchemaRegistration()
-        sr.register(path=cls._SCHEMA_NAME, config_class=config_class)
+        sr.register(path=cls._PRIMARY_SCHEMA_NAME, config_class=primary_schema)
         for group, schema_ls in schemas.items():
             with sr.new_group(group_name=f"schema/{group}", target_path=f"{group}") as group:
                 for info in schema_ls:
