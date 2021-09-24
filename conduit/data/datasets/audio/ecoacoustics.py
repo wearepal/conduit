58d2ce9adbff8b73aca0059059900e19654a4740
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-09-24 11:07:26 +0100

    enum/str check

--- conduit/data/datasets/audio/ecoacoustics.py
+++ conduit/data/datasets/audio/ecoacoustics.py
@@ -84,7 +84,10 @@ class Ecoacoustics(CdtAudioDataset):
         self.ec_labels_path = self.labels_dir / self._EC_LABELS_FILENAME
         self.uk_labels_path = self.labels_dir / self._UK_LABELS_FILENAME
 
-        self.target_attr = target_attr = str_to_enum(str_=target_attr, enum=SoundscapeAttr)
+        if isinstance(target_attr, str):
+            self.target_attr = str_to_enum(str_=target_attr, enum=SoundscapeAttr)
+        else:
+            self.target_attr = target_attr
         self.specgram_segment_len = specgram_segment_len
         self.resample_rate = resample_rate
         self.preprocessing_transform = preprocessing_transform
