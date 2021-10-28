62bbf8f4fd8489664519cafe6cf14a9fa241c92f
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-10-28 15:16:17 +0100

    Remove check.py.

--- check.py
+++
@@ -1,9 +0,0 @@
-import pytorch_lightning as pl
-
-from conduit.data.datamodules.vision.cmnist import ColoredMNISTDataModule
-from conduit.models.self_supervised import MoCoV2
-
-dm = ColoredMNISTDataModule(root="data")
-dm.setup()
-moco = MoCoV2()
-moco.run(datamodule=dm, trainer=pl.Trainer(), copy=False)
