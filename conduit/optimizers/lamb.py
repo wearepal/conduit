3aa6214feef1793d46b8d801360dee0cadab13ab
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-11-02 12:27:43 +0000

    Add LAMB and SAM optimizers.

---
+++ conduit/optimizers/lamb.py
@@ -0,0 +1,138 @@
+import math
+from typing import Callable, Iterable, Optional, Tuple
+
+import torch
+from torch import Tensor
+from torch.optim.optimizer import Optimizer
+
+__all__ = ["LAMB"]
+
+
+class LAMB(Optimizer):
+    r"""
+    Implements the LAMB algorithm introduced in `Large Batch Optimization for Deep Learning`_.
+
+    LAMB serves as the AdamW counterpart to the LARS optimizer, similarly employing layerwise
+    adaptive learning rates train models effectively with large batch sizes.
+
+    .. note::
+        Implementation based on: https://github.com/cybertronai/pytorch-lamb
+
+    .. _Large Batch Optimization for Deep Learning:
+        https://arxiv.org/abs/1904.00962v5
+
+    .. _On the Convergence of Adam and Beyond:
+        https://openreview.net/forum?id=ryQu7f-RZ
+    """
+
+    def __init__(
+        self,
+        params: Iterable[Tensor],
+        lr: float = 1e-3,
+        betas: Tuple[float, float] = (0.9, 0.999),
+        eps: float = 1e-6,
+        weight_decay: float = 0.0,
+        clamp_value: float = 10.0,
+        adam: bool = False,
+        debias: bool = False,
+    ) -> None:
+        """
+        :params: iterable of parameters to optimize or dicts defining parameter groups.
+        :lr: learning rate.
+        :betas: coefficients used for computing running averages of gradient and its square.
+        :eps:: term added to the denominator to improve numerical stability.
+        :weight_decay:: weight decay coefficient.
+        :debias: whether to include the bias-correction term (1 - beta**step) from Adam.
+        """
+        if lr <= 0.0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        if eps < 0.0:
+            raise ValueError(f"Invalid epsilon value: {eps}")
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError(f"Invalid beta parameter at index 0: {betas[0]}")
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError(f"Invalid beta parameter at index 1: {betas[1]}")
+        if weight_decay < 0:
+            raise ValueError(f"Invalid weight_decay value: {weight_decay}")
+        if clamp_value < 0.0:
+            raise ValueError(f"Invalid clamp value: {clamp_value}")
+
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        self.clamp_value = clamp_value
+        self.adam = adam
+        self.debias = debias
+
+        super().__init__(params=params, defaults=defaults)  # type: ignore
+
+    def step(self, closure: Optional[Callable[[], Tensor]] = None) -> Optional[Tensor]:
+        r"""Performs a single optimization step.
+
+        :closure: A closure that reevaluates the model and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    msg = (
+                        "Lamb does not support sparse gradients, "
+                        "please consider SparseAdam instead"
+                    )
+                    raise RuntimeError(msg)
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state["step"] = 0
+                    # Exponential moving average of gradient values
+                    state["exp_avg"] = torch.zeros_like(p)
+                    # Exponential moving average of squared gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(p)
+
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+                beta1, beta2 = group["betas"]
+
+                state["step"] += 1
+
+                # Decay the first and second moment running average coefficient
+                # m_t
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                # v_t
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                # Paper v3 does not use debiasing.
+                if self.debias:
+                    bias_correction = math.sqrt(1 - beta2 ** state["step"])
+                    bias_correction /= 1 - beta1 ** state["step"]
+                else:
+                    bias_correction = 1
+
+                # Apply bias to lr to avoid broadcast.
+                step_size = group["lr"] * bias_correction
+
+                weight_norm = torch.norm(p.data).clamp(0, self.clamp_value)
+
+                adam_step = exp_avg / exp_avg_sq.sqrt().add(group["eps"])
+                if group["weight_decay"] != 0:
+                    adam_step.add_(p.data, alpha=group["weight_decay"])
+
+                adam_norm = torch.norm(adam_step)
+                if weight_norm == 0 or adam_norm == 0:
+                    trust_ratio = 1
+                else:
+                    trust_ratio = weight_norm / adam_norm
+                state["weight_norm"] = weight_norm
+                state["adam_norm"] = adam_norm
+                state["trust_ratio"] = trust_ratio
+                if self.adam:
+                    trust_ratio = 1
+
+                p.data.add_(adam_step, alpha=-step_size * trust_ratio)
+
+        return loss
---
+++ conduit/optimizers/sam.py
@@ -0,0 +1,111 @@
+from typing import Any, Callable, Dict, Iterable
+
+import torch
+from torch import Tensor
+from torch.optim.optimizer import Optimizer
+
+__all__ = ["SAM"]
+
+
+class SAM(Optimizer):
+    """
+    Implements the 'Sharpness Aware Minimization' (SAM) algorithm introducued in
+    `_Sharpness Aware Minimization`_.
+
+    SAM seeks parameters that lie in neighborhoods having uniformly low loss (rather than
+    parameters that only themselves have low loss value) and act as a meta-optimizer, wrapping
+    around some base optimizer.
+
+
+    .. example-code::
+      .. code-block:: python
+          def _closure():
+            return loss_function(output, model(input))
+
+          loss = _closure()
+          loss.backward()
+          optimizer.step(closure=_closure)
+          optimizer.zero_grad()
+
+    .. _Sharpness Aware Minimization:
+        https://arxiv.org/abs/2010.01412
+    """
+
+    def __init__(
+        self,
+        base_optimizer: Optimizer,
+        rho: float = 0.05,
+        adaptive: bool = False,
+    ) -> None:
+        if rho < 0.0:
+            raise ValueError(f"Invalid rho value: {rho}. (Should be non-negative)")
+
+        defaults = dict(rho=rho, adaptive=adaptive)
+        self.base_optimizer = base_optimizer
+        self.param_groups = self.base_optimizer.param_groups
+        super(SAM, self).__init__(params=base_optimizer.param_groups, defaults=defaults)  # type: ignore
+
+    @torch.no_grad()
+    def _first_step(self, zero_grad: bool = False) -> None:
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                self.state[p]["old_p"] = p.data.clone()
+                e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
+                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+
+        if zero_grad:
+            self.zero_grad()
+
+    @torch.no_grad()
+    def _second_step(self, zero_grad: bool = False) -> None:
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                p.data = self.state[p]["old_p"]  # get back to "w" from "w + e(w)"
+
+        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+
+        if zero_grad:
+            self.zero_grad()
+
+    @torch.no_grad()
+    def step(self, closure: Callable[[], Tensor]) -> Tensor:
+        r"""Performs a single optimization step.
+
+        :closure: A closure that reevaluates the model and returns the loss.
+        """
+        self._first_step(zero_grad=True)
+        with torch.enable_grad():
+            loss = closure()
+            loss.backward()
+        self._second_step()
+        return loss
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][
+            0
+        ].device  # put everything on the same device, in case of model parallelism
+        norm = torch.norm(
+            torch.stack(
+                [
+                    ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad)
+                    .norm(p=2)
+                    .to(shared_device)
+                    for group in self.param_groups
+                    for p in group["params"]
+                    if p.grad is not None
+                ]
+            ),
+            p=2,
+        )
+        return norm
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        super().load_state_dict(state_dict)
+        self.base_optimizer.param_groups = self.param_groups
