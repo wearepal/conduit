2d4829a9c08012cc43186505a403d8fcb690a201
Kieran <kg@magmacollective.org>
2022-06-09 11:48:49 +0100

    Fix: allow [m, n] labels over [n,] labels (#148)
    * Fix: allow [m, n] labels over [1, n] labels
    
    * add test to check batch size matches sample dimensions

--- tests/data_test.py
+++ tests/data_test.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 import torch
 from torch import Tensor
-import torchaudio.transforms as AT
+import torchaudio.transforms as AT  # type: ignore
 from torchvision import transforms as T
 from typing_extensions import Type
 
@@ -123,7 +123,7 @@ def test_audio_dataset(root: Path, segment_len: float) -> None:
 
 
 @pytest.mark.slow
-def test_ecoacoustics_labels(root: Path):
+def test_ecoacoustics_metadata_labels(root: Path):
     target_attr = [SoundscapeAttr.habitat]
     ds = Ecoacoustics(
         root=str(root),
@@ -164,6 +164,27 @@ def test_ecoacoustics_dm(root: Path):
     assert test_sample.x.size()[1] == dm.dims[0]
     assert test_sample.x.size()[2] == dm.dims[1]
     assert test_sample.x.size()[3] == dm.dims[2]
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("train_batch_size", [1, 2, 8])
+def test_ecoacoustics_dm_batch_multi_label(root: Path, train_batch_size: int) -> None:
+    target_attrs = [SoundscapeAttr.habitat, SoundscapeAttr.site]
+    data_module = EcoacousticsDataModule(
+        root=str(root),
+        segment_len=30.0,
+        train_batch_size=train_batch_size,
+        target_attrs=target_attrs,
+        train_transforms=AT.Spectrogram(),
+    )
+    data_module.prepare_data()
+    data_module.setup()
+
+    train_dl = data_module.train_dataloader()
+    sample = next(iter(train_dl))
+
+    assert sample.y.size(1) == len(target_attrs)
+    assert sample.x.size(0) == sample.y.size(0) == train_batch_size
 
 
 def test_add_field() -> None:
